e65cdbefb76f22d426281a2bbfa8f2761913f522
Patrick <taaaallen@gmail.com>
2025-11-22 00:44:50 +0100

    Daily commit

--- src/components/RSVPForm/RSVPForm.jsx
+++ src/components/RSVPForm/RSVPForm.jsx
@@ -1,5 +1,5 @@
 import styles from "./RSVPForm.module.css";
-import { useEffect, useState } from "preact/hooks";
+import { useState } from "preact/hooks";
 
 const defaultState = { name: "", plus_one: "", food: "Kött", carpool: "", sleepover: "", arriveFriday: "", songWishlist: "" };
 
@@ -11,68 +11,13 @@ export function RSVPForm() {
   });
 
   // // Regex validations
-  // const nameRegex = /^\D{2,}$/;
-  // const emailRegex = /^[A-Z0-9]+@\w+\.\D{2,}$/i;
-  // const phoneRegex = /^\d{10}$|^\d{3}(\s|-|\.)\d{3}(\s|-|\.)\d{4}$/;
-  // const passwordRegex = /(?=^.{8,}$)((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/;
-
-  // const firstName = form.querySelector("#first-name");
-  // firstName.addEventListener("input", () => {
-  //   const isValid = nameRegex.test(firstName.value);
-  //   isValid ? (firstName.className = "valid") : (firstName.className = "invalid");
-  // });
-
-  // const lastName = form.querySelector("#last-name");
-  // lastName.addEventListener("input", () => {
-  //   const isValid = nameRegex.test(lastName.value);
-  //   isValid ? (lastName.className = "valid") : (lastName.className = "invalid");
-  // });
-
-  // const email = form.querySelector("#email");
-  // email.addEventListener("input", () => {
-  //   const isValid = emailRegex.test(email.value);
-  //   isValid ? (email.className = "valid") : (email.className = "invalid");
-  // });
-
-  // const phone = form.querySelector("#phone");
-  // phone.addEventListener("input", () => {
-  //   const isValid = phoneRegex.test(phone.value);
-  //   isValid ? (phone.className = "valid") : (phone.className = "invalid");
-  // });
-
-  // const password = form.querySelector("#password");
-  // password.addEventListener("input", () => {
-  //   const isValid = passwordRegex.test(password.value);
-  //   isValid ? (password.className = "valid") : (password.className = "invalid error");
-  // });
-
-  // const passwordConfirm = form.querySelector("#password-confirm");
-  // passwordConfirm.addEventListener("input", () => {
-  //   const isValid = passwordRegex.test(passwordConfirm.value) && passwordConfirm.value === password.value;
-  //   isValid ? (passwordConfirm.className = "valid") : (passwordConfirm.className = "invalid error");
-  // });
-
-  // const inputs = form.querySelectorAll("input");
-  // inputs.forEach((input) => {
-  //   input.addEventListener("input", () => {
-  //     const isValid = input.value.length !== 0;
-  //     isValid ? (input.className = "valid") : (input.className = "invalid");
-  //   });
-  // });
-
-  // const passwordInputs = document.querySelectorAll("input[type='password']");
-  // passwordInputs.forEach((input) =>
-  //   input.addEventListener("change", (e) => {
-  //     console.log(e.target.value);
-  //     console.log("e.target.value");
-  //   })
-  // );
+  const nameRegex = /^\D{2,}$/;
 
   const handleChange = (e) => {
     const { name, value } = e.target;
 
     let error = "";
-    if (name === "name" && value.length < 3) {
+    if (name === "name" && !nameRegex.test(value)) {
       error = "That's not a valid name!";
     } else if (value.length < 1) {
       error = "You can't leave this field empty!";
@@ -148,7 +93,6 @@ export function RSVPForm() {
             </label>
           </div>
         </div>
-        {renderValidationFeedback("food")}
       </fieldset>
       <fieldset>
         <legend>Vill du/ni samåka med buss från Malmö till Hotell Mossbylund?</legend>
