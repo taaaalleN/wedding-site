fe5505ac08c464b739fb82c903e7a38ba8188f36
Patrick <patricklindbom@hotmail.com>
2025-12-23 23:28:01 +0100

    Changes to styling and layouts

--- src/components/Timeline/Timeline.jsx
+++ src/components/Timeline/Timeline.jsx
@@ -1,7 +1,8 @@
-import "./style.css";
+// import "./style.css";
+import styles from "./Timeline.module.css";
 import { useEffect, useRef } from "preact/hooks";
 
-export function Timeline() {
+export function Timeline({ title }) {
   const timelineRef = useRef(null);
   const eventsContainerRef = useRef(null);
   const eventRefs = useRef([]);
@@ -34,42 +35,42 @@ export function Timeline() {
   }, []);
 
   return (
-    <div class="timeline__container" ref={timelineRef}>
-      Timeline
-      <div class="timeline">
-        <div class="event" ref={addToRefs}>
+    <div class={styles.timeline__container} ref={timelineRef}>
+      <h3>{title}</h3>
+      <div class={styles.timeline}>
+        <div class={styles.event} ref={addToRefs}>
           <span>Förmingel</span>
           <span>14:30</span>
         </div>
-        <div class="event" ref={addToRefs}>
+        <div class={styles.event} ref={addToRefs}>
           <span>Ceremoni</span>
           <span>15:00</span>
         </div>
-        <div class="event" ref={addToRefs}>
+        <div class={styles.event} ref={addToRefs}>
           <span>Glass & mingel</span>
           <span>15:30</span>
         </div>
-        <div class="event" ref={addToRefs}>
+        <div class={styles.event} ref={addToRefs}>
           <span>Middag</span>
           <span>17:00</span>
         </div>
-        <div class="event" ref={addToRefs}>
+        <div class={styles.event} ref={addToRefs}>
           <span>Tårta</span>
           <span>22:00</span>
         </div>
-        <div class="event" ref={addToRefs}>
+        <div class={styles.event} ref={addToRefs}>
           <span>First dance</span>
           <span>22:30</span>
         </div>
-        <div class="event" ref={addToRefs}>
+        <div class={styles.event} ref={addToRefs}>
           <span>Party</span>
           <span>23:00</span>
         </div>
-        <div class="event" ref={addToRefs}>
+        <div class={styles.event} ref={addToRefs}>
           <span>Körv med bröööd</span>
           <span>00:00</span>
         </div>
-        <div class="event" ref={addToRefs}>
+        <div class={styles.event} ref={addToRefs}>
           <span>Dags att gå hem</span>
           <span>01:00</span>
         </div>
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,27 +1,37 @@
 import styles from "./Home.module.css";
-import couplesImage from "../../assets/usch.jpg";
+import couplesImage from "../../assets/hero_1.jpg";
 
 import { Countdown } from "../../components/Countdown";
+import { Timeline } from "../../components/Timeline/Timeline";
 
 export function Home() {
   return (
     <div class={styles.home}>
       <section class={styles.section__one}>
+        <img class={styles.heroImage} src={couplesImage} alt="Couple's image" />
         <div class="styled">
-          <h1>Laurita & Patrick</h1>
-          <p class={styles.first}>are getting married</p>
-          <p class={styles.second}>on</p>
-          <p class={styles.third}>13th of June 2026</p>
+          <h1>Vi ska gifta oss!</h1>
+          <p>
+            Du som fått vår inbjudan med länken till denna hemsida är en av de personer som, på ett eller annat vis, är
+            de bästa i våra liv. Vi vill förstås att du ska komma till vårt bröllop och dela den dagen med oss!
+          </p>
+          <p>
+            Vi har samlat all information här på hemsidan, på ett och samma ställe för att underlätta för oss alla. Här
+            finner du information om vigsel, fest, boende och praktiska detaljer och lite annat som ni kan tänkas vilja
+            veta.
+          </p>
+          <p>
+            O.S.A. senast den 1 juni, speciellt viktigt för dig som vill bo på Rånäs Slott över natten. Tveka inte att
+            kontakta oss om ni undrar över något. Vi ser fram emot att se er på vårt bröllop!
+          </p>
+          <p>Kram, Laurita & Patrick</p>
         </div>
-        <img src={couplesImage} alt="Couple's image" />
       </section>
       <section class={styles.section__two}>
         <Countdown targetDate={"2026-06-13"} countdownText={"Wedding in..."} completionText={"Wedding time!"} />
       </section>
       <section class={styles.section__three}>
-        <div>
-          <p>Some text</p>
-        </div>
+        <Timeline title="Bröllopsdagen" />
       </section>
     </div>
   );
